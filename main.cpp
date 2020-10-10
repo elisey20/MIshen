fc6090f56108c2237ee65e46c83c607c8d61c5f5
Елисей Ли <elisey.li.2002@mail.ru>
2020-10-10 12:42:28 +1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+float getRandomNumber(int min, int max)
+{
+	
+	
+	return 0;
+}
+
+int main()
+{
+	random_device rd;
+	mt19937 gen(rd());
+
+	float x, y = 0;
+	float SUM = 0;
+	
+	for (int i = 0; i < 5; i++) {
+		
+		
+	}
+
+	return 0;
+}	
