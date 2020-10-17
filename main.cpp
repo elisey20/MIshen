d46e42eb82d12e3f8e7b09c19909c1db700e2881
Елисей Ли <elisey.li.2002@mail.ru>
2020-10-17 14:33:18 +1000

    final

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 ﻿#include <iostream>
 #include <random>
-#include <time.h>
 
 using namespace std;
 
@@ -13,16 +12,46 @@ float getRandomNumber(int min, int max)
 
 int main()
 {
+	
+	//Использование более современного алгоритма псевдорандома
 	random_device rd;
 	mt19937 gen(rd());
 
+	//координаты выстрела
 	float x, y = 0;
-	float SUM = 0;
-	
+	//сумма очков попаданий
+	unsigned short SUM = 0;
+	//очки текущего выстрела
+	short at = 0;
+
 	for (int i = 0; i < 5; i++) {
+		//Ввод координат для выстрела
+		cout << "Enter coordinates of the shot [-5; 5]" << endl << "x = ";
+		cin >> x;
+		cout << "y = ";
+		cin >> y;
+
+		//симуляция разброса выстрела
+		x += (gen() % 1000) / 100.0 - 5;
+		y += (gen() % 1000) / 100.0 - 5;
 		
+		//очки текущего выстрела
+		at = 5 - floor(sqrt(x * x + y * y));
+		at < 0 ? at = 0 : 0;
+
+		//Суммирование очков попадания
+		SUM += at;
+
+		//вы попали в (x; y) и заработали at очков
+		cout << "You hit (" << x << "; " << y << ") and earned " << at << " points\n\n";
 		
 	}
+
+	cout << "for 5 shots you earned " << SUM << " points\n";
+	if (SUM < 10)
+		cout << "LUZER\n";
+	else
+		cout << "You win!\n";
 
 	return 0;
 }	
